5c121035bd58cd3657936e5d1470d90cc6996da7
Luiz Gustavo <lgmds2017@outlook.com>
2023-03-02 16:18:33 -0300

    Fim

--- Supermercado.py
+++ Supermercado.py
@@ -6,47 +6,49 @@ Não permita que o programa quebre com
 erros de índices inexistentes na lista.
 """
 buy_list = []
-options = input('Bem vindo ao Big\n\nSelecione uma opção \n[i]nserir [a]pagar [l]istar [s]air: ').lower()
+options = input(
+    'Bem vindo ao Big\n\nSelecione uma opção \n[i]nserir [a]pagar [l]istar [s]air: ').lower()
 
 
 while True:
     if options == 'i':
-      insert_item = input('Digite o que você vai comprar: ')
-      print('item adicionado com sucesso!')
-      buy_list.insert(0, insert_item)
-      options = input('Selecione uma opção \n[i]nserir [a]pagar [l]istar [s]air: ').lower()
+        insert_item = input('Digite o que você vai comprar: ')
+        print('item adicionado com sucesso!')
+        buy_list.insert(0, insert_item)
+        options = input(
+            'Selecione uma opção \n[i]nserir [a]pagar [l]istar [s]air: ').lower()
 
     elif options == 'a':
-      remove_item = input('Digite qual item você vai apagar: ')
+        remove_item = input('Digite qual item você vai apagar: ')
+        try:
+          # Pelo nome do item
+          buy_list.remove(remove_item)
+          print(f'o item {remove_item} foi removido com sucesso!')
+        except ValueError:
+          print('Item não removido!')
+        except NameError:
+          print('Item não encontrado!')
 
-      # Remover pelo número/posição
-      try:
-        position = int(remove_item)
-        del buy_list[position]
-        print(f'item {buy_list[position]} foi removido com sucesso!')
-      except ValueError:
-        print('Item não removido!')
-      #  Pelo nome do item
-      # buy_list.remove(remove_item)
-
-
-      options = input('Selecione uma opção \n[i]nserir [a]pagar [l]istar [s]air: ').lower()
+        options = input(
+            'Selecione uma opção \n[i]nserir [a]pagar [l]istar [s]air: ').lower()
 
     elif options == 'l':
-      if buy_list == []:
-        print('Sacola vazia!')
-      for i, nome in enumerate(buy_list):
-        print(i, nome)
-      options = input('Selecione uma opção \n[i]nserir [a]pagar [l]istar [s]air: ').lower()
+        if buy_list == []:
+            print('Sacola vazia!')
+        for i, nome in enumerate(buy_list):
+            print(i, nome)
+        options = input(
+            'Selecione uma opção \n[i]nserir [a]pagar [l]istar [s]air: ').lower()
 
     elif options == 's':
-       print('Obrigado por comprar as coisas no Big!')
-       break
+        print('Obrigado por comprar as coisas no Big!')
+        break
 
     elif len(options) > 1:
-       print('um item por vez!')
-       options = input('Selecione uma opção \n[i]nserir [a]pagar [l]istar [s]air: ').lower()
+        print('um item por vez!')
+        options = input(
+            'Selecione uma opção \n[i]nserir [a]pagar [l]istar [s]air: ').lower()
 
     else:
-       print('Selecione apenas as opções abaixo!')
-       options = input('[i]nserir [a]pagar [l]istar [s]air: ').lower()
+        print('Selecione apenas as opções abaixo!')
+        options = input('[i]nserir [a]pagar [l]istar [s]air: ').lower()
